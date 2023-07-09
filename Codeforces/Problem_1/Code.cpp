edce90242a7e10cf69eec47820b679190d53614a
kmtahsin <113965329+kmtahsin@users.noreply.github.com>
2023-07-09 22:53:56 +0600

    Code.cpp

---
+++ Codeforces/Problem_1/Code.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int w;
+    cin>>w;
+    if(w<=100 && w>2)
+    {   if(w%2==0)
+        {
+            cout<<"Yes";
+        }
+        else
+            cout<<"No";
+    }
+    else
+        cout<<"No";
+return 0;
+}
